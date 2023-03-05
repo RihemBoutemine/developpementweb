846f4a09184bd90865bb79efa63ac2540095ee5e
Mona Lisa <mac@MacBook-Pro-de-Mona-Lisa.local>
2023-03-05 21:56:09 +0100

    Initial commit

--- Docker/src/app/__init__.py
+++ Docker/src/app/__init__.py
@@ -30,7 +30,25 @@ def assos():
     return render_template('assos.html', datas=datas)
 
 
+@app.route('/dashboard')
+def dashboard():
+    datas = Data.query.all()
 
+    # Préparer les données pour le graphique Chart.js
+    gestion_count = {}
+    for d in datas:
+        if d.gestion in gestion_count:
+            gestion_count[d.gestion] += 1
+        else:
+            gestion_count[d.gestion] = 1
+
+    gestion_values = list(gestion_count.values())
+    gestion_labels = list(gestion_count.keys())
+    data = {
+        'values': gestion_values,
+        'labels': gestion_labels
+    }
+    return render_template('dashboard.html', graph_data=json.dumps(data))
 
 
 
